abb806b52be69b70b79ed87a962cd02a20439a03
Martin Podloucký <podlouckymartin@gmail.com>
2022-11-01 07:16:38 +0100

    Obsahlejší výchozí data

--- server/lists.js
+++ server/lists.js
@@ -1,16 +1,102 @@
 import { nanoid } from 'nanoid';
 
+const defaultMon = [
+  {
+    id: '2xFyiZo3',
+    product: 'Rohlíky',
+    amount: 10,
+    unit: '',
+    done: true,
+  },
+  {
+    id: 'AUneEMRM',
+    product: 'Máslo',
+    amount: 1,
+    unit: 'ks',
+    done: false,
+  },
+  {
+    id: 'ZeFYF7Ew',
+    product: 'Mléko',
+    amount: 1,
+    unit: 'litr',
+    done: true,
+  },
+  {
+    id: 'NA32X8T8',
+    product: 'Šunka',
+    amount: 2,
+    unit: 'balení',
+    done: false,
+  },
+  {
+    id: 'ok_IbfcM',
+    product: 'Eidem',
+    amount: 1,
+    unit: 'balení',
+    done: false,
+  },
+  {
+    id: 'q82f38bj',
+    product: 'Rajčata',
+    amount: 400,
+    unit: 'g',
+    done: true,
+  },
+  {
+    id: '73tuqqk5',
+    product: 'Okurky',
+    amount: 2,
+    unit: 'ks',
+    done: true,
+  },
+  {
+    id: '1RZrNaxP2',
+    product: 'Cibule',
+    amount: 6,
+    unit: 'ks',
+    done: true,
+  },
+];
+
+const defaultTue = [
+  {
+    id: '-R3z3YxK',
+    product: 'Špagety',
+    amount: 1,
+    unit: 'balení',
+    done: true,
+  },
+  {
+    id: 'TGZn2MZ-',
+    product: 'Mleté maso',
+    amount: 500,
+    unit: 'g',
+    done: true,
+  },
+  {
+    id: 'uYyvQcS5',
+    product: 'Pivo Kozel',
+    amount: 6,
+    unit: '',
+    done: true,
+  },
+  {
+    id: '0KfuJBMLA',
+    product: 'Brambůrky, solené',
+    amount: 2,
+    unit: 'balení',
+    done: false,
+  },
+];
+
 const createDefaultData = () => {
   const result = new Array(52).fill(null).map(_ => ({
     mon: [], tue: [], wed: [], thu: [], fri: [], sat: [], sun: [],
   }));
 
-  result[0].mon = [
-    { id: nanoid(6), product: 'Rohlíky', amount: '10', done: false },
-    { id: nanoid(6), product: 'Máslo', amount: '500 g', done: true },
-    { id: nanoid(6), product: 'Šunka', amount: '200 g', done: true },
-  ];
-
+  result[0].mon = defaultMon;
+  result[0].tue = defaultTue;
   return result;
 };
 
